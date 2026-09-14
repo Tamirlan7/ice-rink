ff326067f01fd225714ac6b471d5bf355ca2b300
Tamirlan7 <zukasaevtamirlan1@gmail.com>
2026-09-14 20:52:25 +0500

    discovery service update

---
+++ eureka-server/src/test/java/by/tami/eurekaserver/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package by.tami.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
