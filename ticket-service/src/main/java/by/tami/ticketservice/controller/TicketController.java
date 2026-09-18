433d702e4d8ac3fb68dc5950e6515b4dc6aaf74c
Tamirlan7 <zukasaevtamirlan1@gmail.com>
2026-09-18 21:09:05 +0500

    separated skate-service and ticket-service config

--- ticket-service/src/main/java/by/tami/ticketservice/controller/TicketController.java
+++ ticket-service/src/main/java/by/tami/ticketservice/controller/TicketController.java
@@ -3,22 +3,28 @@ package by.tami.ticketservice.controller;
 import by.tami.ticketservice.dto.PurchaseTicketArgs;
 import by.tami.ticketservice.service.TicketService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
+@RefreshScope
 @RequestMapping("/api/v1/tickets")
 public class TicketController {
 
-    private TicketService ticketService;
+    @Value("${test.message}")
+    private String message;
+
+    private final TicketService ticketService;
 
     @GetMapping("/test")
     public String test(
             @RequestHeader("X-User-Id") String userId
     ) {
-        return "Hello user with id " + userId + "!";
+        return "Hello user with id " + userId + "! and the message is " + message;
     }
 
     @PostMapping("/purchase")
