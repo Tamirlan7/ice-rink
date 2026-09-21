69031a3cb0ef5e5421251276efd4ea61ca66baf1
Tamirlan7 <zukasaevtamirlan1@gmail.com>
2026-09-21 19:44:26 +0500

    season-service init

---
+++ season-service/src/main/java/by/tami/seasonservice/SeasonServiceApplication.java
@@ -0,0 +1,13 @@
+package by.tami.seasonservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeasonServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SeasonServiceApplication.class, args);
+    }
+
+}
---
+++ season-service/src/test/java/by/tami/seasonservice/SeasonServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package by.tami.seasonservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeasonServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
